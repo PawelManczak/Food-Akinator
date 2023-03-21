023653678ef299c5521773d8feef1a0fe9e836a6
kasiaOsowska <96206203+kasiaOsowska@users.noreply.github.com>
2023-03-21 14:32:59 +0100

    adding files

---
+++ database.py
@@ -0,0 +1,14 @@
+database=[
+            {"name": "spaghetti carbonara", "salty": 1, "bitter": 0, "spicy": 0.25, "cheese": 1, "pasta": 1, "cuisine": "Italian"},
+            {"name": "pizza", "salty": 1, "bitter": 0.25, "spicy": 0.25, "cheese": 1, "cuisine": "Italian"},
+            {"name": "dumplings", "salty": 1, "bitter": 0, "spicy": 0.5, "cheese": 0.5, "cuisine": "Polish"},
+            {"name": "lasagna", "salty": 1, "bitter": 0, "spicy": 0.25, "cheese": 1, "pasta": 0.5, "cuisine": "Italian"},
+            {"name": "pad thai", "salty": 1, "bitter": 0.25, "spicy": 1, "cheese": 0, "cuisine": "Thai"},
+            {"name": "sushi", "salty": 1, "bitter": 0, "spicy": 0.25, "cheese": 0, "cuisine": "Japanese"},
+            {"name": "tacos", "salty": 1, "bitter": 0.25, "spicy": 1, "cheese": 0.5, "cuisine": "Mexican"},
+            {"name": "tiramisu", "sweet": 1, "coffee": 1, "alcohol": 0.5, "cocoa": 1, "cuisine": "Italian"},
+            {"name": "creme brulee", "sweet": 1, "creamy": 1, "cuisine": "French"},
+            {"name": "churros", "sweet": 1, "crispy": 1, "chocolate": 0.5, "cuisine": "Spanish"},
+            {"name": "mochi", "sweet": 1, "chewy": 1, "bean paste": 1, "cuisine": "Japanese"},
+            {"name": "baklava", "sweet": 1, "crispy": 1, "cuisine": "Arabic/Balkan"},
+      ]
