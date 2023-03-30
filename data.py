1140613aaa01fcd81812e246b84cfd46155b72e7
PawelManczak <pawel14522@gmail.com>
2023-03-30 20:45:36 +0200

    dataHandler class, creating optimal tree

--- data.py
+++ data.py
@@ -1,22 +1,137 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 
 
 @dataclass
-class SetUpData:
-    string_arg: str
+class Dish:
     sweet: bool = False
+    sour: bool = False
     spicy: bool = False
+    salty: bool = False
+    umami: bool = False
     bitter: bool = False
     cheese: bool = False
+    gluten_free: bool = False
+    vegetarian: bool = False
+    vegan: bool = False
+    organic: bool = False
+    cuisineAmerican: bool = False
+    cuisineChinese: bool = False
+    cuisineFrench: bool = False
+    cuisineGreek: bool = False
+    cuisineIndian: bool = False
     cuisineItalian: bool = False
+    cuisineJapanese: bool = False
+    cuisineKorean: bool = False
+    cuisineMexican: bool = False
+    cuisineMiddle_Eastern: bool = False
+    cuisineRussian: bool = False
+    cuisineThai: bool = False
+    cuisineVietnamese: bool = False
+    cuisineOther: bool = False
 
     def __post_init__(self):
-        true_args = [arg for arg in (self.sweet, self.spicy, self.bitter, self.cheese) if arg]
+        true_args = [arg for arg in locals() if arg]
         if len(true_args) == 0:
             raise ValueError("At least one bool argument must be True.")
-"""example of usage:
-example = SetUpData('some string', sweet=True, spicy=True)
-print(example)
-output:
-SetUpData(string_arg='some string', sweet=True, spicy=True, bitter=False, cheese=False)
-"""
+
+    def get_dish_vector(dish):
+        dish_fields = fields(Dish)
+        return [getattr(dish, field.name) for field in dish_fields]
+
+    @staticmethod
+    def get_dish_variable_names():
+        dish_fields = fields(Dish)
+        return [field.name for field in dish_fields]
+
+
+class DataHandler:
+    names: list[str] = []
+    dishes = []
+
+    def add_dish(self, name: str, dish: Dish):
+        self.names.append(name)
+        self.dishes.append(dish.get_dish_vector())
+
+    def __init__(self):
+        self.load_dishes()
+
+    def load_dishes(self):
+        self.add_dish('pizza margherita', Dish(cheese=True, cuisineItalian=True))
+        self.add_dish('pizza capriciosa',
+                      Dish(sweet=False, sour=False, spicy=False, salty=False, umami=False, bitter=False, cheese=True,
+                           gluten_free=False, vegetarian=False, vegan=False, organic=False, cuisineAmerican=False,
+                           cuisineChinese=False, cuisineFrench=False, cuisineGreek=False, cuisineIndian=False,
+                           cuisineItalian=True, cuisineJapanese=False, cuisineKorean=False, cuisineMexican=False,
+                           cuisineMiddle_Eastern=False, cuisineRussian=False, cuisineThai=False,
+                           cuisineVietnamese=False, cuisineOther=False))
+        self.add_dish('spaghetti bolognese',
+                      Dish(sweet=False, sour=False, spicy=False, salty=False, umami=False, bitter=False, cheese=False,
+                           gluten_free=False, vegetarian=False, vegan=False, organic=False, cuisineAmerican=False,
+                           cuisineChinese=False, cuisineFrench=False, cuisineGreek=False, cuisineIndian=False,
+                           cuisineItalian=True, cuisineJapanese=False, cuisineKorean=False, cuisineMexican=False,
+                           cuisineMiddle_Eastern=False, cuisineRussian=False, cuisineThai=False,
+                           cuisineVietnamese=False, cuisineOther=False))
+        self.add_dish('carbonara',
+                      Dish(sweet=False, sour=False, spicy=False, salty=False, umami=False, bitter=False, cheese=True,
+                           gluten_free=False, vegetarian=False, vegan=False, organic=False, cuisineAmerican=False,
+                           cuisineChinese=False, cuisineFrench=False, cuisineGreek=False, cuisineIndian=False,
+                           cuisineItalian=True, cuisineJapanese=False, cuisineKorean=False, cuisineMexican=False,
+                           cuisineMiddle_Eastern=False, cuisineRussian=False, cuisineThai=False,
+                           cuisineVietnamese=False, cuisineOther=False))
+        self.add_dish('lasagne',
+                      Dish(sweet=False, sour=False, spicy=False, salty=False, umami=False, bitter=False, cheese=True,
+                           gluten_free=False, vegetarian=False, vegan=False, organic=False, cuisineAmerican=False,
+                           cuisineChinese=False, cuisineFrench=False, cuisineGreek=False, cuisineIndian=False,
+                           cuisineItalian=True, cuisineJapanese=False, cuisineKorean=False, cuisineMexican=False,
+                           cuisineMiddle_Eastern=False, cuisineRussian=False, cuisineThai=False,
+                           cuisineVietnamese=False, cuisineOther=False))
+        self.add_dish('risotto',
+                      Dish(sweet=False, sour=False, spicy=False, salty=False, umami=False, bitter=False, cheese=True,
+                           gluten_free=False, vegetarian=False, vegan=False, organic=False, cuisineAmerican=False,
+                           cuisineChinese=False, cuisineFrench=False, cuisineGreek=False, cuisineIndian=False,
+                           cuisineItalian=True, cuisineJapanese=False, cuisineKorean=False, cuisineMexican=False,
+                           cuisineMiddle_Eastern=False, cuisineRussian=False, cuisineThai=False,
+                           cuisineVietnamese=False, cuisineOther=False))
+        self.add_dish('hamburger',
+                      Dish(sweet=False, sour=False, spicy=False, salty=False, umami=False, bitter=False, cheese=True,
+                           gluten_free=False, vegetarian=False, vegan=False, organic=False, cuisineAmerican=True,
+                           cuisineChinese=False, cuisineFrench=False, cuisineGreek=False, cuisineIndian=False,
+                           cuisineItalian=False, cuisineJapanese=False, cuisineKorean=False, cuisineMexican=False,
+                           cuisineMiddle_Eastern=False, cuisineRussian=False, cuisineThai=False,
+                           cuisineVietnamese=False, cuisineOther=False))
+        self.add_dish('steak', Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('salmon fillet', Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('fish and chips',
+                      Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=False))
+        self.add_dish('chicken curry', Dish(sweet=False, spicy=True, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('beef stroganoff',
+                      Dish(sweet=False, spicy=False, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('spinach and feta pie',
+                      Dish(sweet=False, spicy=False, sour=False, bitter=True, salty=True, umami=False))
+        self.add_dish('grilled cheese sandwich',
+                      Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('hamburger', Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('chicken shawarma',
+                      Dish(sweet=False, spicy=True, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('fajitas', Dish(sweet=False, spicy=True, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('taco salad', Dish(sweet=False, spicy=False, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('chicken teriyaki',
+                      Dish(sweet=True, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('beef and broccoli stir fry',
+                      Dish(sweet=False, spicy=False, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('vegetable lasagna',
+                      Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('beef bourguignon',
+                      Dish(sweet=False, spicy=False, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('shrimp scampi', Dish(sweet=False, spicy=False, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('lamb chops', Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('chicken cordon bleu',
+                      Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('chicken alfredo',
+                      Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('spaghetti bolognese',
+                      Dish(sweet=False, spicy=False, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('pork ribs', Dish(sweet=True, spicy=True, sour=False, bitter=False, salty=True, umami=True))
+        self.add_dish('beef kebab', Dish(sweet=False, spicy=True, sour=True, bitter=False, salty=True, umami=True))
+        self.add_dish('grilled salmon',
+                      Dish(sweet=False, spicy=False, sour=False, bitter=False, salty=True, umami=True))
--- main.py
+++ main.py
@@ -1,41 +1,30 @@
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.tree import export_text
-from database import database
-from data import SetUpData
 
-# enums
-# DecisionTreeClassifier() cannot work on strings, so we need to set to each of them identifier,
-# we should make it programmatically
+from data import DataHandler, Dish
 
-# creating a list from database structure
-values_list = []
-for recipe in database:
-    values = list(recipe.values())
-    values_list.append(values)
-
-#print(values_list)
+dh = DataHandler()
 
 # creating a decision tree
-x = values_list
-y = list(range(0, len(x)))
+x = dh.dishes
+y = dh.names
 
 tree_clf = DecisionTreeClassifier()
 tree_clf.fit(x, y)
 
 # visualization
-features = list(database[0].keys())
+
 f = open("iris_tree.dot", 'w')
 export_graphviz(
     tree_clf,
     out_file=f,  # path where you want it to output
-    feature_names=features,
-    class_names=list(map(str, y)),
+    feature_names=Dish.get_dish_variable_names(),
+    class_names=y,
     rounded=True,
     filled=True
 )
 
 r = export_text(tree_clf)
-#print(r)
-example = SetUpData('some string', sweet=True, spicy=True)
-print(example)
+print(r)
+
 # dot -Tpng iris_tree.dot -o iris_tree.png <-- command exporting to png
