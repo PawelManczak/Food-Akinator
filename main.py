4781c38ba12f5770bee1cbe6a6303f0ca06ebb90
PawelManczak <pawel14522@gmail.com>
2023-04-04 08:51:50 +0200

    Possibility to answer "I don't know"

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.tree import export_text
 
@@ -8,58 +10,89 @@ dh = DataHandler()
 # creating a decision tree
 x = dh.dishes
 y = dh.names
+x_df = pd.DataFrame(x)
+y_df = pd.DataFrame(dh.names)
+counter = 0
 print(len(x))
-tree_clf = DecisionTreeClassifier(criterion='entropy', min_samples_leaf=1, max_depth=100000)
-tree_clf.fit(x, y)
+feature_names = Dish.get_dish_variable_names()
+feature_names_df = pd.DataFrame(feature_names)
+answered = False
+x_df = x_df.iloc[1:].reset_index(drop=True)
+y_df = y_df.iloc[1:].reset_index(drop=True)
+x_df.columns = x_df.columns.astype(str)
 
-# visualization
-f = open("iris_tree.dot", 'w')
-export_graphviz(
-    tree_clf,
-    out_file=f,  # path where you want it to output
-    feature_names=Dish.get_dish_variable_names(),
-    class_names=y,
-    rounded=True,
-    filled=True
-)
+while not answered:
+    tree_clf = DecisionTreeClassifier(criterion='entropy', min_samples_leaf=1)
+    tree_clf.fit(x_df, y_df)
 
-r = export_text(tree_clf)
-print(r)
+    # visualization
+    f = open("iris_tree.dot", 'w')
+    export_graphviz(
+        tree_clf,
+        out_file=f,  # path where you want it to output
+        feature_names=feature_names,
+        class_names=y,
+        rounded=True,
+        filled=True
+    )
+    r = export_text(tree_clf)
+    # print(r)
 
-children_left = tree_clf.tree_.children_left
-children_right = tree_clf.tree_.children_right
+    children_left = tree_clf.tree_.children_left
+    children_right = tree_clf.tree_.children_right
 
-#print(tree_clf.tree_.feature[6])
-# pobieramy tablice z indeksami atrybutów i wartościami progów
-features = tree_clf.tree_.feature
-thresholds = tree_clf.tree_.threshold
+    # print(tree_clf.tree_.feature[6])
+    # pobieramy tablice z indeksami atrybutów i wartościami progów
+    features = tree_clf.tree_.feature
+    thresholds = tree_clf.tree_.threshold
 
 
-# wyświetlamy warunki różnicujące dla węzłów
+    # wyświetlamy warunki różnicujące dla węzłów
 
-# # funkcja rekurencyjna do wypisania węzłów
-def print_node(node_id, depth):
-    # wypisujemy węzeł
+    # # funkcja rekurencyjna do wypisania węzłów
+    def print_node(node_id, depth):
+        global x, counter, feature_names_df, x_df, answered, feature_names
+        # wypisujemy węzeł
+        if children_left[node_id] == -1 or children_right[node_id] == -1:
 
-    if children_left[node_id] == -1 or children_right[node_id] == -1:
+            predicted_class = tree_clf.classes_[tree_clf.tree_.value[node_id][0].argmax()]
+            print("Klasa końcowa:", predicted_class)
+            answered = True
 
-        predicted_class = tree_clf.classes_[tree_clf.tree_.value[node_id][0].argmax()]
-        print("Klasa końcowa:", predicted_class)
+        else:
 
-    else:
+            choice = "L"
 
-        choice = "L"
+            while choice != "t" and choice != "f":
+                print("{}{}. Czy wybrana potrawa jest {}".format(
+                    "  " * depth, node_id, list(feature_names_df.values)[features[node_id]]))
+                # przechodzimy do lewego i prawego potomka, jeśli istnieją
 
-        while(choice != "t" and choice != "f"):
-            print("{}{}. Czy wybrana potrawa jest {}".format(
-                "  " * depth, node_id, Dish.get_dish_variable_names()[features[node_id]], thresholds[node_id]))
-            # przechodzimy do lewego i prawego potomka, jeśli istnieją
+                choice = input()
 
-            choice = input()
+                if choice == "f":
+                    print_node(children_left[node_id], depth + 1)
+                elif choice == "t":
+                    print_node(children_right[node_id], depth + 1)
+                else:
 
-            if choice == "f":
-                print_node(children_left[node_id], depth + 1)
-            elif choice == "t":
-                print_node(children_right[node_id], depth + 1)
+                    # usuwanie kolumny z feature_names_x i x _df
 
-print_node(0, 0)
+                    column_to_remove = features[node_id]  # indeks kolumny do usunięcia
+                    feature_names = [f for i, f in enumerate(feature_names) if i != column_to_remove]
+                    feature_names_df = pd.DataFrame(feature_names)
+
+                    x_df = x_df.drop(str(column_to_remove), axis=1).reset_index(drop=True)
+                    x_df.columns = x_df.columns.astype(int)
+
+                    new_headers = [i for i in range(len(x_df.columns))]
+
+                    # zmiana nazw kolumn na nowe nagłówki
+                    x_df = x_df.rename(columns=dict(zip(x_df.columns, new_headers)))
+                    x_df.columns = x_df.columns.astype(str)
+                    return
+
+
+    print_node(0, 0)
+
+# dot -Tpng iris_tree.dot -o iris_tree.png <-- command exporting to png
